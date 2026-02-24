113bda2ef9df146e7b0ca93fbbe01aad0cfc4741
ShahzaibKhan0210 <162992008+ShahzaibKhan0210@users.noreply.github.com>
2026-02-24 23:47:19 +0500

    Refactor: Project files updated

--- src/test/java/runners/LoginRunner.java
+++ src/test/java/runners/LoginRunner.java
@@ -6,7 +6,12 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "stepdefinitions",
-        plugin = {"pretty", "html:target/cucumber-reports.html"},
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports/cucumber.html",
+                "json:target/cucumber-reports/cucumber.json",
+                "junit:target/cucumber-reports/cucumber-junit.xml"
+        },
         tags = ""
 )
 public class LoginRunner extends AbstractTestNGCucumberTests {
